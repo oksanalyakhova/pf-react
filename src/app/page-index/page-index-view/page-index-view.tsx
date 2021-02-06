03b922ca40af525ee84caff9e7532c395e2c2ff9
oksanalyakhova <o.v.lyakhova@gmail.com>
2021-02-06 16:56:42 +0200

    finished hello layout & styles

--- src/app/page-index/page-index-view/page-index-view.tsx
+++ src/app/page-index/page-index-view/page-index-view.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
+import { theme } from '../../../helpers/variables';
 import { FlexUnit } from '../../../helpers/mixins';
 // import breakpoint from 'styled-components-breakpoint';
 
@@ -20,28 +21,53 @@ const StyledMain = styled.main`{
 
 
 const StyledSectionHello = styled.section`{
+  position: relative;
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
+  ${FlexUnit(30, 80, 300, 'vw', 'font-size')};
+  text-transform: uppercase;
 }`
 
 const StyledO = styled.div`{
-  
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.variables.cBg};
+  background-color: ${theme.variables.cText};
 }`
 
-const Hello = styled.div`{
-  
+const StyledHello = styled.div`{
+  position: absolute;
+  top: 0;
+  left: 100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.variables.cText};
+  background-color: ${theme.variables.cBg};
 }`
 
 const PageIndex: FunctionComponent<PageIndexProps> = ({
     text
 }): JSX.Element => {
+
+
+
     return (
         <StyledMain className="main">
             <StyledSectionHello className="hello">
                 <StyledO className="hello__preview">O</StyledO>
-                <Hello className="hello__content">
+                <StyledHello className="hello__content">
                     Hello
-                </Hello>
+                </StyledHello>
             </StyledSectionHello>
         </StyledMain>
     )
